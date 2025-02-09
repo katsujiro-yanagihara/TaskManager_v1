889f0bfcde80427470673f5c60ef38d6a10916ad
katsujiro-yanagihara <katsujiro.yanagihara@gmail.com>
2025-02-09 11:31:16 +0900

    Remove debug logs

--- pages/index.js
+++ pages/index.js
@@ -87,8 +87,6 @@ const TaskManager = () => {
       ));
       setEditingDateTaskId(null);
     } else {
-      setSelectedDateTime(dateTime);
-      setHasTime(useTime);
       const e = new Event('submit');
       e.preventDefault = () => {};
       addTask(e, dateTime, useTime);
